e36dc454e2a4c310eb261ae301c5ae8c0fbfd0ed
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-02-02 06:29:45 +0100

    Update my_streamlit_datathon.py

--- my_streamlit_datathon.py
+++ my_streamlit_datathon.py
@@ -30,27 +30,27 @@ col1, col2 = st.columns([1, 1], gap="large")
 
 with col1:
     st.write("Choix du premier partenaire:")
-    dining_1 = st.slider("Dining (Partenaire 1)", min_value=0, max_value=10, value=None, step=1)
-    gaming_1 = st.slider("Gaming (Partenaire 1)", min_value=0, max_value=10, value=None, step=1)
-    clubbing_1 = st.slider("Clubbing (Partenaire 1)", min_value=0, max_value=10, value=None, step=1)
-    reading_1 = st.slider("Reading (Partenaire 1)", min_value=0, max_value=10, value=None, step=1)
-    shopping_1 = st.slider("Shopping (Partenaire 1)", min_value=0, max_value=10, value=None, step=1)
-    Sports_1 = st.slider("Sports (Partenaire 1)", min_value=0, max_value=10, value=None, step=1)
-    Art_1 = st.slider("Art (Partenaire 1)", min_value=0, max_value=10, value=None, step=1)
-    Musique_1 = st.slider("Musique (Partenaire 1)", min_value=0, max_value=10, value=None, step=1)
-    TV_Cinema_1 = st.slider("TV Cinema (Partenaire 1)", min_value=0, max_value=10, value=None, step=1)
+    dining_1 = st.slider("Dining (Partenaire 1)", min_value=1, max_value=10, value=None, step=1)
+    gaming_1 = st.slider("Gaming (Partenaire 1)", min_value=1, max_value=10, value=None, step=1)
+    clubbing_1 = st.slider("Clubbing (Partenaire 1)", min_value=1, max_value=10, value=None, step=1)
+    reading_1 = st.slider("Reading (Partenaire 1)", min_value=1, max_value=10, value=None, step=1)
+    shopping_1 = st.slider("Shopping (Partenaire 1)", min_value=1, max_value=10, value=None, step=1)
+    Sports_1 = st.slider("Sports (Partenaire 1)", min_value=1, max_value=10, value=None, step=1)
+    Art_1 = st.slider("Art (Partenaire 1)", min_value=1, max_value=10, value=None, step=1)
+    Musique_1 = st.slider("Musique (Partenaire 1)", min_value=1, max_value=10, value=None, step=1)
+    TV_Cinema_1 = st.slider("TV Cinema (Partenaire 1)", min_value=1, max_value=10, value=None, step=1)
 
 with col2:
     st.write("Choix du second partenaire:")
-    dining_2 =st.slider("Dining (Partenaire 2)", min_value=0, max_value=10, value=None, step=1)
-    gaming_2 =st.slider("Gaming (Partenaire 2)", min_value=0, max_value=10, value=None, step=1)
-    clubbing_2 = st.slider("Clubbing (Partenaire 2)", min_value=0, max_value=10, value=None, step=1)
-    reading_2  =st.slider("Reading (Partenaire 2)", min_value=0, max_value=10, value=None, step=1)
-    shopping_2 =st.slider("Shopping (Partenaire 2)", min_value=0, max_value=10, value=None, step=1)
-    Sports_2 =st.slider("Sports (Partenaire 2)", min_value=0, max_value=10, value=None, step=1)
-    Art_2 =st.slider("Art (Partenaire 2)", min_value=0, max_value=10, value=None, step=1)
-    Musique_2 = st.slider("Musique(Partenaire 2)", min_value=0, max_value=10, value=None, step=1)
-    TV_Cinema_2 =st.slider("TV_Cinema (Partenaire 2)", min_value=0, max_value=10, value=None, step=1)
+    dining_2 =st.slider("Dining (Partenaire 2)", min_value=1, max_value=10, value=None, step=1)
+    gaming_2 =st.slider("Gaming (Partenaire 2)", min_value=1, max_value=10, value=None, step=1)
+    clubbing_2 = st.slider("Clubbing (Partenaire 2)", min_value=1, max_value=10, value=None, step=1)
+    reading_2  =st.slider("Reading (Partenaire 2)", min_value=1, max_value=10, value=None, step=1)
+    shopping_2 =st.slider("Shopping (Partenaire 2)", min_value=1, max_value=10, value=None, step=1)
+    Sports_2 =st.slider("Sports (Partenaire 2)", min_value=1, max_value=10, value=None, step=1)
+    Art_2 =st.slider("Art (Partenaire 2)", min_value=1, max_value=10, value=None, step=1)
+    Musique_2 = st.slider("Musique(Partenaire 2)", min_value=1, max_value=10, value=None, step=1)
+    TV_Cinema_2 =st.slider("TV_Cinema (Partenaire 2)", min_value=1, max_value=10, value=None, step=1)
 
 # Préparer les données pour le modèle
 user_1_input = np.array([[dining_1, gaming_1, clubbing_1, reading_1, shopping_1, Sports_1, Art_1, Musique_1, TV_Cinema_1]])
@@ -80,24 +80,75 @@ similarity = cosine_similarity(user_1_input, user_2_input)
 st.write("Similarité cosinus entre les deux utilisateurs :", similarity[0][0])
 
 
-# # Système de recommandation :
-# st.write("Voici nos recommandations en fonction des critères choisis :")
+link1 = '/content/drive/MyDrive/Crew Data BLEU [ADATAR]/PROJETS/PROJET 2/Basile, Clara, Costin/Data/Cleaned/tmdb_full_cleaned.csv'
+df_tmdb = pd.read_csv(link1)
 
+link3 = '/content/drive/MyDrive/Crew Data BLEU [ADATAR]/PROJETS/PROJET 2/Basile, Clara, Costin/Data/Cleaned/title_basics_cleaned.csv'
+df_imdb = pd.read_csv(link3)
 
-# def recommandation(df):
+df_imdb.loc[df_imdb['originalTitle'].str.contains('Amélie')]
 
-#     col0, col1= st.columns(2)
+films_rom_imdb = df_imdb.loc[df_imdb['genres'].str.contains('Romance')]
 
+films_rom_imdb['runtimeMinutes'] = films_rom_imdb['runtimeMinutes'].replace(r"\N", None)
+films_rom_imdb['runtimeMinutes'] = pd.to_numeric(films_rom_imdb['runtimeMinutes'])
+films_rom_imdb = films_rom_imdb.loc[films_rom_imdb['runtimeMinutes'] >= 60]
 
-#     with col0:
-#         full_link_0="https://image.tmdb.org/t/p/w500" +films_rom['poster_path'][0]
-#         st.image(full_link_0)
-#         imdb_link_0 = "https://www.imdb.com/title/"+films_rom['imdb_id'][0]
-#         st.write(f"[{ films_rom['originalTitle'][1]}]({imdb_link_0})")
+films_rom_tmdb = df_tmdb.loc[df_tmdb['genres'].str.contains('Romance')]
 
+films_rom_tmdb = films_rom_tmdb.loc[films_rom_tmdb['runtime'] >= 60]
 
-#     with col1:
-#         full_link_1="https://image.tmdb.org/t/p/w500" +films_rom['poster_path'][1]
-#         st.image(full_link_1)
-#         imdb_link_1 = "https://www.imdb.com/title/"+films_rom['imdb_id'][1]
-#         st.write(f"[{ recommandation['originalTitle'][2]}]({imdb_link_1})")
+films_select = films_rom_tmdb.loc[films_rom_tmdb['popularite_ponderee'] >= 6]
+
+films_full = pd.merge(films_rom_imdb, films_select, how='inner', right_on= 'imdb_id', left_on= 'tconst')
+
+films_rom = films_full.loc[films_full['popularite_ponderee'] >= 6.45]
+
+films_rom.drop(['tconst', 'primaryTitle', 'endYear', 'backdrop_path', 'budget',
+                'homepage', 'imdb_id', 'original_language', 'original_title',
+                'overview','popularity','production_countries', 'tagline',
+                'video', 'vote_average', 'vote_count',
+                'production_companies_name', 'runtimeMinutes', 'genres_x',
+                'production_companies_country', 'release_date', 'revenue', 'titleType', 'title'], axis = 1, inplace=True)
+
+films_rom.sort_values(by= 'popularite_ponderee', ascending= False)
+
+df_0 = films_rom.loc[films_rom['popularite_ponderee'] > 7.5]
+
+df_20 = films_rom.loc[(films_rom['popularite_ponderee'] > 6.8) & (films_rom['popularite_ponderee'] <= 7.5)]
+
+df_40 = films_rom.loc[(films_rom['popularite_ponderee'] > 6.608) & (films_rom['popularite_ponderee'] <= 6.8)]
+
+df_60 = films_rom.loc[(films_rom['popularite_ponderee'] > 6.51) & (films_rom['popularite_ponderee'] <= 6.607)]
+
+df_80 = films_rom.loc[films_rom['popularite_ponderee'] < 6.51]
+
+if similarity < 0.19:
+  print(df_0)
+elif 0.20 <= similarity < 0.39:
+  print(df_20)
+elif  0.40 <= similarity < 0.59:
+  print(df_40)
+elif 0.60 <= similarity < 0.79:
+  print(df_60)
+else :
+  print(df_80)
+
+# Système de recommandation :
+st.write("Voici nos recommandations en fonction des critères choisis :")
+
+def recommandation(df):
+
+    col0, col1= st.columns(2)
+
+    with col0:
+        full_link_0="https://image.tmdb.org/t/p/w500" +films_rom['poster_path'][0]
+        st.image(full_link_0)
+        imdb_link_0 = "https://www.imdb.com/title/"+films_rom['imdb_id'][0]
+        st.write(f"[{ films_rom['originalTitle'][1]}]({imdb_link_0})")
+
+    with col1:
+        full_link_1="https://image.tmdb.org/t/p/w500" +films_rom['poster_path'][1]
+        st.image(full_link_1)
+        imdb_link_1 = "https://www.imdb.com/title/"+films_rom['imdb_id'][1]
+        st.write(f"[{ recommandation['originalTitle'][2]}]({imdb_link_1})")
