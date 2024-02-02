932aec6cdb0842e4d7fd034c49c1062d1759bbeb
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-02-02 11:21:55 +0100

    Update my_streamlit_datathon.py

--- my_streamlit_datathon.py
+++ my_streamlit_datathon.py
@@ -90,13 +90,13 @@ X_train_selected, X_test_selected, y_train, y_test = train_test_split(X_selected
 model = LogisticRegression().fit(X_train, y_train)
 
 # Probabilité de prédiction pour les classes
-    resultat =[]
-    for i, j in zip(model.classes_, model.predict_proba(X_test)[0]*100):
-        resultat.append(j)
-    if resultat[0]>resultat[1]:
-        st.write("Pas Match Aîe aîe aîe, ça sent le sapin entre vous !")
-    else:
-        st.write("Match Chaude soirée en perspective !")
+resultat =[]
+for i, j in zip(model.classes_, model.predict_proba(X_test)[0]*100):
+    resultat.append(j)
+if resultat[0]>resultat[1]:
+    st.write("Pas Match Aîe aîe aîe, ça sent le sapin entre vous !")
+else:
+    st.write("Match Chaude soirée en perspective !")
 
 # Calcul de la similarité cosinus entre les deux utilisateurs
 similarity = cosine_similarity(user_1_input, user_2_input)
