0284ef4909136d122bfd22cf195e4a6d02f07d2e
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-02-02 04:59:41 +0100

    Update my_streamlit_datathon.py

--- my_streamlit_datathon.py
+++ my_streamlit_datathon.py
@@ -29,7 +29,7 @@ from sklearn.metrics import accuracy_score, r2_score
 # from sklearn.metrics import confusion_matrix
 
 # # Import dataset
-# link = "https://github.com/The-Pandwa/Datathon/blob/main/df_final_speed_dating.csv"
+# link = "https://github.com/The-Pandwa/Datathon/blob/main/df_final_speed_dating_test.csv"
 # df_final_speed_dating = pd.read_csv(link)
 
 # Création de la sidebar et features
