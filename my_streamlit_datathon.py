8ad9822506ff8c348028e3ef07da26b7d7b18776
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-02-02 09:18:12 +0100

    Update my_streamlit_datathon.py

--- my_streamlit_datathon.py
+++ my_streamlit_datathon.py
@@ -92,7 +92,7 @@ tmdb_full_cleaned_1 = pd.read_csv(link10)
 link11 = "https://raw.githubusercontent.com/The-Pandwa/Datathon/main/tmdb_full_cleaned_2.csv"
 tmdb_full_cleaned_2 = pd.read_csv(link11)
 
-df_tmdb = pd.concat([tmdb_full_cleaned_1, tmdb_full_cleaned_2]) 
+df_tmdb = pd.concat([tmdb_full_cleaned_1, tmdb_full_cleaned_2])
 
 link3 = 'https://raw.githubusercontent.com/The-Pandwa/Datathon/main/title_basics_cleaned.csv'
 df_imdb = pd.read_csv(link3)
