309241ffdc3aa0b728f03dfff246998a29dae664
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-02-02 05:01:08 +0100

    Update my_streamlit_datathon.py

--- my_streamlit_datathon.py
+++ my_streamlit_datathon.py
@@ -29,8 +29,8 @@ from sklearn.metrics import accuracy_score, r2_score
 # from sklearn.metrics import confusion_matrix
 
 # # Import dataset
-# link = "https://github.com/The-Pandwa/Datathon/blob/main/df_final_speed_dating_test.csv"
-# df_final_speed_dating = pd.read_csv(link)
+link = "https://github.com/The-Pandwa/Datathon/blob/main/df_final_speed_dating_test.csv"
+df_final_speed_dating = pd.read_csv(link)
 
 # Création de la sidebar et features
 st.sidebar.title('Votre taux de compatibilité :')
